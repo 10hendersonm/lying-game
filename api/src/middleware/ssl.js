a2caf0ba7d47b7820186beb2cbbc0cc1eefcad94
Marty Henderson <10hendersonm@gmail.com>
2019-06-30 14:26:13 -0500

    added new challenge string

--- api/src/middleware/ssl.js
+++ api/src/middleware/ssl.js
@@ -1,11 +1,21 @@
 import { Router } from 'express'
 
 export default () => {
-  const { SSL_ACME_STRING, SSL_ACME_CHALLENGE } = process.env
+  const {
+    SSL_ACME_STRING,
+    SSL_ACME_CHALLENGE,
+    SSL_ACME_STRING2,
+    SSL_ACME_CHALLENGE2,
+  } = process.env
   const app = Router()
   app.get(`/.well-known/acme-challenge/${SSL_ACME_STRING}`, (req, res) => {
     res.contentType('application/force-download')
     res.send(SSL_ACME_CHALLENGE)
+  })
+
+  app.get(`/.well-known/acme-challenge/${SSL_ACME_STRING2}`, (req, res) => {
+    res.contentType('application/force-download')
+    res.send(SSL_ACME_CHALLENGE2)
   })
   return app
 }
